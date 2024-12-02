92c0b2eb98120bb25e5c7dfa6bdf8071d22b7b22
Chris Kjærsig Opstrup <chris.opstrup@bestseller.com>
2024-12-02 14:12:19 +0100

    Convert to es6 modules

--- example/index.js
+++ example/index.js
@@ -1,20 +1,21 @@
-var assert = require('assert')
+import Simplex from "../index.js";
 
-var Simplex = require('..')
+const simplex = new Simplex();
 
-var simplex = new Simplex()
+for (let y = 40; y > 0; y--) {
+  let line = "";
+  for (let x = 80; x > 0; x--) {
+    line += valToChar(simplex.noise(x / 50, y / 50));
+  }
+  console.log(line);
+}
 
-for (var y = 40; y > 0; y--) {
-  var line = ''
-  for (var x = 80; x > 0; x--) {
-    line += valToChar(simplex.noise(x/50, y/50))
-  };
-  console.log(line)
-};
-
-function valToChar (val) {
-  var colors = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,"^`\'. '.split('')
-  var nVal = (val+1)/2
-  var idx = Math.floor(nVal * colors.length)
-  return colors[idx]
+function valToChar(val) {
+  const colors =
+    "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ".split(
+      ""
+    );
+  const nVal = (val + 1) / 2;
+  const idx = Math.floor(nVal * colors.length);
+  return colors[idx];
 }
